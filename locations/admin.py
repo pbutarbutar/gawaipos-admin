e146812c05426a4d921fa65f69717f02bed175c5
pbutarbutar <paru.liano@gmail.com>
2022-06-10 14:12:29 +0700

    Add Location

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from locations.models import Country,State,City,Street
 
 # Register your models here.
+
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Street)
