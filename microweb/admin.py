5adc3fb2feba7e0bd10c47a7031ecb083d4b266f
pbutarbutar <paru.liano@gmail.com>
2022-07-07 13:38:18 +0700

    Add Role merchan on web micro

--- microweb/admin.py
+++ microweb/admin.py
@@ -21,6 +21,31 @@ class AboutUsAdmin(SummernoteModelAdmin):
         })
     )
     summerenote_fields =('body',)
+
+    def get_queryset(self, request):
+
+        # Override the get_queryset method for Admin
+        qs = super(AboutUsAdmin, self).get_queryset(request)
+        
+        merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+        print("debug :", merchant_acc.role_user)
+
+        if merchant_acc.role_user == "GAWAIPOS": 
+            return qs
+
+        #if request.user.is_superuser:
+          #  return qs
+        return qs.filter(merchant = merchant_acc.merchant)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "merchant":
+            merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+            print("debug :", merchant_acc.merchant_id)
+
+            if merchant_acc.role_user != "GAWAIPOS": 
+                kwargs['queryset'] = Merchant.objects.filter(id=merchant_acc.merchant_id)
+
+        return super(AboutUsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
     
 class ProductListAdmin(SummernoteModelAdmin):
 
@@ -29,12 +54,62 @@ class ProductListAdmin(SummernoteModelAdmin):
     search_fields = ('product_title', )
     summerenote_fields =('product_view_body',)
 
+    def get_queryset(self, request):
+
+        # Override the get_queryset method for Admin
+        qs = super(ProductListAdmin, self).get_queryset(request)
+        
+        merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+        print("debug :", merchant_acc.role_user)
+
+        if merchant_acc.role_user == "GAWAIPOS": 
+            return qs
+
+        #if request.user.is_superuser:
+          #  return qs
+        return qs.filter(merchant = merchant_acc.merchant)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "merchant":
+            merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+            print("debug :", merchant_acc.merchant_id)
+
+            if merchant_acc.role_user != "GAWAIPOS": 
+                kwargs['queryset'] = Merchant.objects.filter(id=merchant_acc.merchant_id)
+
+        return super(ProductListAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 class ProductListImagesAdmin(SummernoteModelAdmin):
 
     list_display = ('merchant', 'product_category','slug', 'product_images_title', 'product_images_body', 'product_images',  'is_draft', 'created_by', 'created_at')
     list_filter = ('is_draft', 'created_at', 'merchant_id')
     search_fields = ('product_title', )
     summerenote_fields =('product_images_body',)
+
+    def get_queryset(self, request):
+
+        # Override the get_queryset method for Admin
+        qs = super(ProductListImagesAdmin, self).get_queryset(request)
+        
+        merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+        print("debug :", merchant_acc.role_user)
+
+        if merchant_acc.role_user == "GAWAIPOS": 
+            return qs
+
+        #if request.user.is_superuser:
+          #  return qs
+        return qs.filter(merchant = merchant_acc.merchant)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "merchant":
+            merchant_acc = AccountMerchant.objects.filter(user_id=request.user).first()
+            print("debug :", merchant_acc.merchant_id)
+
+            if merchant_acc.role_user != "GAWAIPOS": 
+                kwargs['queryset'] = Merchant.objects.filter(id=merchant_acc.merchant_id)
+
+        return super(ProductListImagesAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 class ProfileAdmin(SummernoteModelAdmin):
     list_display = ('merchant', 'slug', 'title', 'name_business', 'is_active', 'created_by', 'created_at')
